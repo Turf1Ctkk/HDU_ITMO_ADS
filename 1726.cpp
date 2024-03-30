be33b18e3a84c421ef085d5c28a31a1a47920dd7
Turf1Ctkk <147962215+Turf1Ctkk@users.noreply.github.com>
2024-03-30 19:21:37 +0800

    Add files via upload

---
+++ 1726.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <deque>
+#include <sstream>
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <ctime>
+using namespace std;
+typedef long long LL;
+typedef double DB;
+#define max(a,b) ((a)>(b)?(a):(b))
+#define min(a,b) ((a)<(b)?(a):(b))
+#define For(i, s, t) for(int i = (s); i <= (t); i++)
+#define Ford(i, s, t) for(int i = (s); i >= (t); i--)
+#define Rep(i, t) for(int i = (0); i < (t); i++)
+#define Repd(i, t) for(int i = ((t)-1); i >= (0); i--)
+#define rep(i, s, t) for(int i = (s); i < (t); i++)
+
+inline int read(){
+    int x=0,f=1;char c=getchar();
+    while(!isdigit(c)){if(c=='-')f=-1;c=getchar();}
+    while(isdigit(c)){x=x*10+c-'0';c=getchar();}
+    return x*f;
+}
+
+int main()
+{
+    LL res=0;
+    int n=read();
+    int x[n], y[n];
+    Rep(i,n) x[i]=read(),y[i]=read();
+    sort(x,x+n);sort(y,y+n);
+    Rep(i,n){
+        LL dis=x[i+1]+y[i+1]-x[i]-y[i];
+        dis*=(i+1);dis*=(n-i-1);              //multiply together will WA #10 :(
+        res+=dis;
+    }
+    res*=2;res/=n;res/=(n-1);
+    printf("%lld", res);
+    return 0;
+}
